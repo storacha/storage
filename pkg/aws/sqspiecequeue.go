87343e0f1b84f31ee97593a6bbf80a9974122f41
hannahhoward <hannah@hannahhoward.net>
2024-11-11 10:23:47 +0700

    feat(aws): add aws tooling and setup

---
+++ pkg/aws/sqspiecequeue.go
@@ -0,0 +1,70 @@
+package aws
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/ipfs/go-cid"
+	cidlink "github.com/ipld/go-ipld-prime/linking/cid"
+	"github.com/storacha/go-piece/pkg/piece"
+	"github.com/storacha/storage/pkg/pdp/aggregator"
+)
+
+// PieceLinkMessage is the struct that is serialized onto an SQS message queue in JSON
+type PieceLinkMessage struct {
+	Piece string `json:"piece,omitempty"`
+}
+
+// SQSPieceQueue implements the providercacher.CachingQueue interface using SQS
+type SQSPieceQueue struct {
+	queueURL  string
+	sqsClient *sqs.Client
+}
+
+// NewSQSPieceQueue returns a new SQSCachingQueue for the given aws config
+func NewSQSPieceQueue(cfg aws.Config, queurURL string) *SQSPieceQueue {
+	return &SQSPieceQueue{
+		queueURL:  queurURL,
+		sqsClient: sqs.NewFromConfig(cfg),
+	}
+}
+
+// Queue implements blobindexlookup.CachingQueue.
+func (s *SQSPieceQueue) Queue(ctx context.Context, piece piece.PieceLink) error {
+
+	messageJSON, err := json.Marshal(PieceLinkMessage{Piece: piece.Link().String()})
+	if err != nil {
+		return fmt.Errorf("serializing message json: %w", err)
+	}
+	_, err = s.sqsClient.SendMessage(ctx, &sqs.SendMessageInput{
+		QueueUrl:    aws.String(s.queueURL),
+		MessageBody: aws.String(string(messageJSON)),
+	})
+	if err != nil {
+		return fmt.Errorf("enqueueing message: %w", err)
+	}
+	return nil
+}
+
+var _ aggregator.QueuePieceAggregationFn = (&SQSPieceQueue{}).Queue
+
+// DecodePieceMessage extracts a piece link from an SQS queue messagebody
+func DecodePieceMessage(messageBody string) (piece.PieceLink, error) {
+	var msg PieceLinkMessage
+	err := json.Unmarshal([]byte(messageBody), &msg)
+	if err != nil {
+		return nil, fmt.Errorf("deserializing message: %w", err)
+	}
+	c, err := cid.Decode(msg.Piece)
+	if err != nil {
+		return nil, fmt.Errorf("decoding link: %w", err)
+	}
+	piece, err := piece.FromLink(cidlink.Link{Cid: c})
+	if err != nil {
+		return nil, fmt.Errorf("decoding piece link: %w", err)
+	}
+	return piece, nil
+}
