46c3e1625880885212f1541132885f501e0b0499
Alan Shaw <alan@storacha.network>
2024-11-12 15:27:50 +0700

    fix: remove read on write check

--- pkg/service/storage/ucan.go
+++ pkg/service/storage/ucan.go
@@ -141,15 +141,6 @@ func NewUCANServer(storageService Service, options ...server.Option) (server.Ser
 						return blob.AllocateOk{}, nil, failure.FromError(err)
 					}
 
-					a, err := storageService.Blobs().Allocations().List(ctx, cap.Nb().Blob.Digest)
-					if err != nil {
-						return blob.AllocateOk{}, nil, failure.FromError(err)
-					}
-					if len(a) < 1 {
-						return blob.AllocateOk{}, nil, failure.FromError(errors.New("failed to read allocation after write"))
-					}
-					log.Info("successfully read allocation after write")
-
 					return blob.AllocateOk{Size: size, Address: address}, nil, nil
 				},
 			),
