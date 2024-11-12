ce5f45a28b17f4eb709997a8c0fde6e6d1e84a18
Alan Shaw <alan@storacha.network>
2024-11-12 17:29:35 +0700

    fix: fork s3 ds

--- pkg/store/allocationstore/datastore.go
+++ pkg/store/allocationstore/datastore.go
@@ -3,7 +3,6 @@ package allocationstore
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"github.com/ipfs/go-datastore"
 	"github.com/ipfs/go-datastore/query"
@@ -21,9 +20,6 @@ func (d *DsAllocationStore) List(ctx context.Context, digest multihash.Multihash
 	pfx := digestutil.Format(digest)
 	results, err := d.data.Query(ctx, query.Query{Prefix: pfx})
 	if err != nil {
-		if strings.Contains(err.Error(), "NoSuchKey") {
-			return []allocation.Allocation{}, nil
-		}
 		return nil, fmt.Errorf("querying datastore: %w", err)
 	}
 
