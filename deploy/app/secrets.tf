2b81d95307b3316b3e358b6362eb380e1e153018
Alan Shaw <alan@storacha.network>
2025-02-04 15:13:00 +0000

    fix: only add secret if using external blob bucket

--- deploy/app/secrets.tf
+++ deploy/app/secrets.tf
@@ -10,6 +10,7 @@ resource "aws_ssm_parameter" "private_key" {
 }
 
 resource "aws_ssm_parameter" "external_blob_bucket_access_key_id" {
+  count       = var.use_external_blob_bucket ? 1 : 0
   name        = "/${var.app}/${terraform.workspace}/Secret/EXTERNAL_BLOB_BUCKET_ACCESS_KEY_ID/value"
   description = "access key ID for an externally hosted blob bucket"
   type        = "SecureString"
@@ -21,6 +22,7 @@ resource "aws_ssm_parameter" "external_blob_bucket_access_key_id" {
 }
 
 resource "aws_ssm_parameter" "external_blob_bucket_secret_access_key" {
+  count       = var.use_external_blob_bucket ? 1 : 0
   name        = "/${var.app}/${terraform.workspace}/Secret/EXTERNAL_BLOB_BUCKET_SECRET_ACCESS_KEY/value"
   description = "secret access key for an externally hosted blob bucket"
   type        = "SecureString"
