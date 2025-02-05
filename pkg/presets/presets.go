5c8daf77dbabe04e844e61d0d97b3b439edf9fb0
Alan Shaw <alan@storacha.network>
2025-02-05 15:06:24 +0000

    fix: add legacy dids in mapping

--- pkg/presets/presets.go
+++ pkg/presets/presets.go
@@ -13,5 +13,7 @@ var (
 	PrincipalMapping      = map[string]string{
 		"did:web:staging.up.storacha.network": "did:key:z6MkqVThfb3PVdgT5yxumxjFFjoQ2vWd26VUQKByPuSB9N91",
 		"did:web:up.storacha.network":         "did:key:z6MkmbbLigYdv5EuU9tJMDXXUudbySwVNeHNqhQGJs7ALUsF",
+		"did:web:staging.up.web3.storage":     "did:key:z6MkqVThfb3PVdgT5yxumxjFFjoQ2vWd26VUQKByPuSB9N91",
+		"did:web:up.web3.storage":             "did:key:z6MkmbbLigYdv5EuU9tJMDXXUudbySwVNeHNqhQGJs7ALUsF",
 	}
 )
