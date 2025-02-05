b0b021f9e5c46a7ec4c4866c1104310e64c1782a
Alan Shaw <alan@storacha.network>
2025-02-05 15:56:53 +0000

    fix: public key for staging upload service

--- pkg/presets/presets.go
+++ pkg/presets/presets.go
@@ -11,9 +11,9 @@ var (
 	IndexingServiceDID, _ = did.Parse("did:web:indexer.storacha.network")
 	IndexingServiceURL, _ = url.Parse("https://indexer.storacha.network")
 	PrincipalMapping      = map[string]string{
-		"did:web:staging.up.storacha.network": "did:key:z6MkqVThfb3PVdgT5yxumxjFFjoQ2vWd26VUQKByPuSB9N91",
+		"did:web:staging.up.storacha.network": "did:key:z6MkhcbEpJpEvNVDd3n5RurquVdqs5dPU16JDU5VZTDtFgnn",
 		"did:web:up.storacha.network":         "did:key:z6MkmbbLigYdv5EuU9tJMDXXUudbySwVNeHNqhQGJs7ALUsF",
-		"did:web:staging.web3.storage":        "did:key:z6MkqVThfb3PVdgT5yxumxjFFjoQ2vWd26VUQKByPuSB9N91",
+		"did:web:staging.web3.storage":        "did:key:z6MkhcbEpJpEvNVDd3n5RurquVdqs5dPU16JDU5VZTDtFgnn",
 		"did:web:web3.storage":                "did:key:z6MkmbbLigYdv5EuU9tJMDXXUudbySwVNeHNqhQGJs7ALUsF",
 	}
 )
